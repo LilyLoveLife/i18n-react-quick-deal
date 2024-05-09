d7e4195ad6a93963510d8682910efb6d8373b846
Lily <lily.song@hrtps.com>
2024-05-09 21:19:41 +0800

    feat: expect.extend

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+// import type {Config} from 'jest';
 
-import type {Config} from 'jest';
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+  
+// };
 
-const config: Config = {
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -26,7 +28,6 @@ const config: Config = {
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-  verbose: true,
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -79,16 +80,16 @@ const config: Config = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -103,7 +104,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -147,7 +148,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment:  "node", //"jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -156,10 +157,10 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -176,19 +177,23 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    // ...
+    "^.+\.(js|ts|tsx)$": 'ts-jest', //"<rootDir>/node_modules/babel-jest",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
+  silent: false,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,20 @@
-declare module 'expect' {
-    interface AsymmetricMatchers {
-        toBeChineseEqual(): void;
-    }
-    interface Matchers<R> {
-        toBeChineseEqual(): R;
-    }
-}
+// declare module 'expect' {
+//     interface AsymmetricMatchers {
+//         toBeChineseEqual(): void;
+//         toBeAbove(argument: number): void;
+//     }
+//     interface Matchers<R> {
+//         toBeChineseEqual(): R;
+//         toBeAbove(argument: number): R;
+//     }
+//     // interface PromiseMatchers{
+//     //     toBeChineseEqual(): void;
+//     // }
+//     // interface ExtendedMatchers extends jest.Matchers<void, number> {
+//     //     toBeAbove: (expected: number) => object;
+//     // }
+// }
+// interface ExtendedMatchers extends jest.Matchers<void, number> {
+//     toBeAbove: (expected: number) => object;
+// }
+
